0de378b19e1a52b0b92bb53da6229e7d0c8085b5
Sameer <saameerx77@gmail.com>
2025-11-20 10:03:57 +0530

    fixed bugs

---
+++ GadgetHub.Application/Common/Mappings/MapsterConfig.cs
@@ -0,0 +1,29 @@
+﻿using GadgetHub.Application.DTOs.Auth;
+using GadgetHub.Application.DTOs.Categories;
+using GadgetHub.Application.DTOs.Products;
+using GadgetHub.Domain.Entities;
+using Mapster;
+
+namespace GadgetHub.Application.Common.Mappings
+{
+    public static class MapsterConfig
+    {
+        public static void RegisterMappings()
+        {
+            var config = TypeAdapterConfig.GlobalSettings;
+
+            // Product mappings
+            config.NewConfig<Product, ProductDto>()
+                  .Map(dest => dest.CategoryName, src => src.Category.Name);
+
+            config.NewConfig<CreateProductDto, Product>();
+            config.NewConfig<UpdateProductDto, Product>();
+            config.NewConfig<RegisterRequestDto, User>();
+            
+            // Category mappings
+            config.NewConfig<Category, CategoryDto>();
+            config.NewConfig<CreateCategoryDto, Category>();
+            config.NewConfig<UpdateCategoryDto, Category>();
+        }
+    }
+}
